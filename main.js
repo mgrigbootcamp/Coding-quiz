b7ca7487a373a5441d02214b1c525d213c9e5efd
Matt Griggs <golf4griggs47@gmail.com>
2021-06-21 13:56:48 -0700

    First changes

---
+++ main.js
@@ -0,0 +1,23 @@
+var questions= [];
+var secondsLeft= 60;
+var highScores= [];
+var score= 0;
+var questionIndex= 0;
+//Target elements in html and store variables here
+
+function startQuiz (){
+    //Add display none to start page
+    // Remove style attribute from questions div
+    //Start timer
+    //Call display question function 
+}
+function displayQuestion (){
+    //Target question title element on page and change text content
+    //var questionTitle= questions[questionIndex].title
+    //Write a loop to create all the buttons
+    //
+
+}
+function checkAnswer (){
+    
+}
