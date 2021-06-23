e0b63e6fb724b4872e93f3440c4e0d14789edc22
Matt Griggs <golf4griggs47@gmail.com>
2021-06-23 14:40:38 -0700

    More changes

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-var questions= [];
+var questions= [
+    {
+        title:"CSS is used primarily to:", 
+        choices: ["Style a webpage", "Store variables", "Use event listeners", "Store data"],
+        answer: "Style a webpage"
+    },
+    {
+        title: "Which of the following is not a Javascrpit data type?",
+        choices: ["Boolean", "Class", "Number", "String"],
+        answer: "Class"
+    },
+    {
+        title:"Boostrap is a CSS library:",
+        choices: ["True", "False"],
+        answer: "True",
+    }
+];
+displayquestionIndex();
 var secondsLeft= 60;
 var highScores= [];
 var score= 0;
 var questionIndex= 0;
+var timer;
+var timerEl=document.querySelector(".timer");
+var startButtonEl=document.querySelector(".start-button")
+startButtonEl.addEventListener("click", startQuiz);
 //Target elements in html and store variables here
 
 function startQuiz (){
-    //Add display none to start page
-    // Remove style attribute from questions div
-    //Start timer
-    //Call display question function 
+    var startPage= document.querySelector (".start-page");
+    startPage.setAttribute("style","display:none");
+    var questionsPage=document.querySelector(".questions-page");
+    questionsPage.removeAttribute("style");
+    timer=setInterval(function(){
+        secondsLeft--
+        timerEl.textContent=secondsLeft
+    }, 1000)
+    displayQuestion()
+    
+    
 }
 function displayQuestion (){
+    var questionTitle= questions[questionIndex].title
     //Target question title element on page and change text content
     //var questionTitle= questions[questionIndex].title
     //Write a loop to create all the buttons
@@ -19,5 +48,16 @@ function displayQuestion (){
 
 }
 function checkAnswer (){
-    
+    questionIndex++
+    displayQuestion ()
+
+}
+function quizEnd (){
+
+}
+function displayHighscores (){
+
+}
+function submitHighscore (){
+
 }
